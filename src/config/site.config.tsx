a56b8511cdb47de02f17039f3078c31fd47a7296
anna.somik <kduzkdkuzkdkd@gmail.com>
2025-09-03 20:25:03 +0600

    feat: add navigation bar component

---
+++ src/config/site.config.tsx
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: "Рецепты вкусной и полезной еды",
+  description: "Сайт с пп рецептами",
+  navItems: [
+    { href: "/", label: "Рецепты" },
+    { href: "/ingredients", label: "Ингредиенты" },
+    { href: "/about", label: "О нас" },
+  ],
+};
