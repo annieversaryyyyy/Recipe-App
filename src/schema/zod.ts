0f21b3357e4909c9494bb8d3dc4c9df8cb6d2149
anna.somik <kduzkdkuzkdkd@gmail.com>
2025-10-10 17:53:27 +0600

    fixed zod

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -1,15 +1,17 @@
 import { object, string, number } from "zod";
 import { z } from "zod";
 
+
 export const signInSchema = object({
-  email: z
-    .string()
-    .min(1, { message: "Email is required" })
-    .email({ message: "Email is invalid" }),
-  password: z
-    .string()
-    .min(6, { message: "Password must be at least 8 characters long" }),
+  email: string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(6, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters")
 });
+
 
 export const ingredientSchema = z.object({
   name: string().min(1, "Название обязательно"),
