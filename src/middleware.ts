d3a9e3143d5fa3a20eca12155589be2d47fde4a7
anna.somik <kduzkdkuzkdkd@gmail.com>
2025-10-10 16:13:44 +0600

    feat: add create and edit recipe functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,41 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { getToken, GetTokenParams } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const token = await getToken({
+
+  let params: GetTokenParams = {
     req: request,
-    secret: process.env.AUTH_SECRET ?? "secret",
-  });
+    secret: process.env.AUTH_SECRET ?? "secret"
+  };
 
-  const protectedRoutes = ["/ingredients"];
+  if (process.env.NODE_ENV === "production") {
+    params = {
+      ...params,
+      cookieName: "__Secure-authjs.session-token"
+    };
+  }
 
-  if (protectedRoutes.some((route) => pathname.startsWith(route))) {
+  const token = await getToken(params);
+
+  const protectedRoutes = ["/ingredients", "/recipes/new", "/recipes/:path*"];
+
+  if (
+    protectedRoutes.some((route) =>
+      pathname.startsWith(route.replace(":path*", ""))
+    )
+  ) {
     if (!token) {
       const url = new URL("/error", request.url);
       url.searchParams.set("message", "Недостаточно прав");
       return NextResponse.redirect(url);
     }
   }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/ingredients"],
+  matcher: ["/ingredients", "/recipes/new", "/recipes/:path*"]
 };
